79e97f0a381b9f203c4102c69b492b4ba796f970
FRANCISMUNGANGU <123382836+FRANCISMUNGANGU@users.noreply.github.com>
2023-10-16 14:12:50 +0300

    Create conditionals.c

---
+++ conditionals.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char username, password;
+    username = "Joseph";
+    password = "1234";
+    if(username == password)
+    {
+    printf("Password and username are similar, choose another password");
+    }
+    else{
+    printf("Welcome");
+    }
+    return 0;
+}
